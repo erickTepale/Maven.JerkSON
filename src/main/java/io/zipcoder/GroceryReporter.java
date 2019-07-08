dd73622adafae3fc3fe5cf70950db064ba09b9a8
Erick Tepale <e.tepale@gmail.com>
2019-07-08 16:45:26 -0400

    finished all parsing, just need to print out the grocery ticket

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,16 +1,31 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+import io.zipcoder.utils.ItemParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GroceryReporter {
     private final String originalFileText;
+    private List<Item> list;
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
+        this.list = new ArrayList<>();
     }
 
     @Override
     public String toString() {
+        try {
+            list = new ItemParser().parseItemList(originalFileText);
+        } catch (ItemParseException e) {
+            e.printStackTrace();
+        }
+
+        //create map<String, Integer> map that counts item names and one that counts item prices :( out of time.
         return null;
     }
 }
