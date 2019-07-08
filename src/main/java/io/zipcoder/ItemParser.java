352e7f3bd8dc661aed584d8a7f789513105a406e
Erick Tepale <e.tepale@gmail.com>
2019-07-08 15:39:14 -0400

    finished happy path

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,50 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
-    public List<Item> parseItemList(String valueToParse) {
-        return null;
+
+
+    public List<Item> parseItemList(String valueToParse) throws ItemParseException {
+        List<Item> items = new ArrayList<>();
+        Pattern pattern = Pattern.compile("([^#]*)##"); // idk why but it works!
+        Matcher matcher = pattern.matcher(valueToParse);
+        for(int i = 0; matcher.find(); i++){
+            items.add(parseSingleItem(matcher.group()));
+        }
+
+
+        return items;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        String name = "";
+        Double price = 0.0;
+        String type = "";
+        String expiration = "";
+
+        //Pattern pattern = Pattern.compile("[:@^*%]");
+        Pattern pattern = Pattern.compile("[:@^*%]([^;]*)[;|##]"); // matches everything between :@^*% and ;
+        Matcher matcher = pattern.matcher(singleItem);
+        for (int i = 0; matcher.find(); i++) {
+            if(i == 0) name = trim(matcher.group()).toLowerCase();
+            else if (i == 1) price = Double.parseDouble(trim(matcher.group()));
+            else if (i==2) type = trim(matcher.group()).toLowerCase();
+            else if(i == 3) expiration = trim(matcher.group());
+            else System.out.println("error parsing?");
+        }
+
+        return new Item(name, price, type, expiration);
+    }
+
+    public String trim(String word){
+        if(word.charAt(word.length() - 2) == '#'){
+            return word.substring(1, word.length()-2);
+        }
+        return word.substring(1, word.length() - 1);
     }
 }
---
+++ src/test/java/io/zipcoder/erickTestsParser/parseKeyValue.java
@@ -0,0 +1,81 @@
+package io.zipcoder.erickTestsParser;
+
+
+import io.zipcoder.ItemParser;
+import io.zipcoder.utils.Item;
+import io.zipcoder.utils.ItemParseException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests parsing 1 key value to object
+ *
+ */
+public class parseKeyValue {
+
+    @Test
+    public void test1() throws ItemParseException {
+        // given
+        ItemParser itemParser = new ItemParser();
+        String valueToParse = new StringBuilder()
+                .append("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##")
+                .toString();
+        Item item1 = new Item("milk", 3.23, "food", "1/25/2016");
+
+
+        // when
+        Item actualList = itemParser.parseSingleItem(valueToParse);
+
+        // then
+        assertEquals(item1, actualList);
+    }
+
+    @Test
+    public void test2() throws ItemParseException {
+
+            String text = "The Quick Brown Fox";
+            String patternString = ".";
+            Pattern pattern = Pattern.compile(patternString);
+            Matcher matcher = pattern.matcher(text);
+            for (int i = 0; matcher.find(); i++) {
+                System.out.println(new StringBuilder()
+                        .append("\n-------------------")
+                        .append("\nValue = " + matcher.group())
+                        .append("\nMatch Number = " + i)
+                        .append("\nStarting index = " + matcher.start())
+                        .append("\nEnding index = " + matcher.end())
+                        .toString());
+
+            }
+
+    }
+
+    @Test
+    public void testMatcher() throws ItemParseException {
+        ItemParser a = new ItemParser();
+        String valueToParse = new StringBuilder()
+                .append("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##")
+                .append("NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##")
+                .append("NAMe:eggs;price:1.24;type:Food;expiration:2/25/2016##")
+                .append("NAMe:BacoN;price:1.25;type:Food;expiration:2/25/2016##")
+                .toString();
+
+        a.parseItemList(valueToParse);
+    }
+
+    @Test
+    public void testTrim(){
+        ItemParser a = new ItemParser();
+
+        Assert.assertEquals("Milk", a.trim("^Milk;"));
+        Assert.assertEquals("Milk", a.trim("%Milk##"));
+    }
+}
