97331c98cc46fc4f95dc9b36b58b887a7cee5e26
Erick Tepale <e.tepale@gmail.com>
2019-07-08 16:07:49 -0400

    3 tests left to pass

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -9,14 +9,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ItemParser {
-
+    public static Integer wackEntry = 0;
+    public static Integer totalMatches = 0;
 
     public List<Item> parseItemList(String valueToParse) throws ItemParseException {
         List<Item> items = new ArrayList<>();
         Pattern pattern = Pattern.compile("([^#]*)##"); // idk why but it works!
         Matcher matcher = pattern.matcher(valueToParse);
         for(int i = 0; matcher.find(); i++){
-            items.add(parseSingleItem(matcher.group()));
+            Item temp = parseSingleItem(matcher.group());
+            if(temp != null) items.add(temp);
         }
 
 
@@ -32,15 +34,26 @@ public class ItemParser {
         //Pattern pattern = Pattern.compile("[:@^*%]");
         Pattern pattern = Pattern.compile("[:@^*%]([^;]*)[;|##]"); // matches everything between :@^*% and ;
         Matcher matcher = pattern.matcher(singleItem);
-        for (int i = 0; matcher.find(); i++) {
-            if(i == 0) name = trim(matcher.group()).toLowerCase();
-            else if (i == 1) price = Double.parseDouble(trim(matcher.group()));
-            else if (i==2) type = trim(matcher.group()).toLowerCase();
-            else if(i == 3) expiration = trim(matcher.group());
-            else System.out.println("error parsing?");
+        try {
+            for (int i = 0; matcher.find(); i++) {
+                if (i == 0){ name = trim(matcher.group()).toLowerCase(); totalMatches++;}
+                else if (i == 1) {price = Double.parseDouble(trim(matcher.group())); totalMatches++;}
+                else if (i == 2){ type = trim(matcher.group()).toLowerCase(); totalMatches++;}
+                else if (i == 3) {expiration = trim(matcher.group()); totalMatches++;}
+                else{ System.out.println("error parsing?"); }
+            }
+            if (totalMatches.equals(0))
+                throw new ItemParseException();
+            if (name.equals("") || price.equals(0.0) || type.equals("") || expiration.equals(""))
+                return null;
+
+            return new Item(name, price, type, expiration);
+        }catch (Exception e){
+            wackEntry++;
+            throw new ItemParseException();
         }
 
-        return new Item(name, price, type, expiration);
+       //return null;
     }
 
     public String trim(String word){
